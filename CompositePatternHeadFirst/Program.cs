24924558938995942232c1aaee9e98d149b07332
unknown <s.granet@TSC-843.techsmith.com>
2014-03-06 09:04:39 -0500

    Added some stuff to the hotfix branch

--- CompositePatternHeadFirst/Program.cs
+++ CompositePatternHeadFirst/Program.cs
@@ -52,7 +52,7 @@ namespace CompositePatternHeadFirst
          var rootMenu = new Menu("Root", "Holds other menus");
          rootMenu.Add(pizzaMenu);
          rootMenu.Add(saladMenu);
-        // rootMenu.Add(iceCreamMenu);
+        // rootMenu.Add(iceCreamMenu);  // lets make some changes
          //rootMenu.Add(steakMenu);
 
          // Send the root menu to the waitress 
